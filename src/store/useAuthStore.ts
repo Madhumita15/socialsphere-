ccf73121265f48ee07d032a607996e9aaf783a41
Madhumita15 <madhumita011das@gmail.com>
2026-04-29 19:49:58 +0530

    changes on useAuthStore

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -199,7 +199,7 @@ export const useAuthStore = create<AuthStore>((set) => ({
       const { error } = await supabase.auth.signInWithOAuth({
         provider: "google",
         options: {
-          redirectTo: "http://localhost:3000/auth/callback",
+          redirectTo: `${window.location.origin}/auth/callback`,
         },
       });
       if (error) throw error;
